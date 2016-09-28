e5adfeaabc5cd493a17ce24bf63ce341f0732518
coffeecarsten <jacobnovum@gmail.com>
2016-09-29 01:02:28 +0200

    edited stockScrape.py and made it almost work. Need to fix problem with multiple regex mathces...

--- stockScrape.py
+++ stockScrape.py
@@ -3,18 +3,7 @@
 import urllib
 import re
 
-print "Content-type:text/html\r\n\r\n"
-print "<head><title>test</title></head>"
-print "<html>"
-print "<body>"
-print '<table border="1">'
-print "<tr>"
-print "<td>SYMB</td>"
-print "<td>pris</td>"
-print "<td>procent</td>"
-print "</tr>"
-
-symbolslist = ["fls.co","aapl","novo-b.co","nzym-b.co","tdc.co","msft","dis","ko","vow.f","nesr.f","g4s.co","danske.co"]
+symbolslist = ["fls:cph","aapl","novo-b.co","nzym-b.co","tdc.co","msft","dis","ko","vow.f","nesr.f","g4s.co","danske.co"]
 ######novo split jan 2, 2014 i 1/5
 ######apple split jun 9, 2014 i 1/7
 ########## g4s.co har jeg sat til 25 men det er ikke rigtigt. ved ikke hvad jeg kobte til.
@@ -22,25 +11,14 @@ symbolslist = ["fls.co","aapl","novo-b.co","nzym-b.co","tdc.co","msft","dis","ko
 orgpricelist= [316,86,159,163,43,25,98,37,183,57,25,180]
 #styk=[]
 i=0
-####print "|	SYMB	|	BUY	|	'%'		| "
+####print "|    SYMB    |   BUY |   '%'     | "
 while i<len(symbolslist):
-	url = "https://finance.yahoo.com/q;_ylt=AsYRfIsgdDn56eKmf2ut2k4nv7gF?uhb=uhb2&fr=uh3_finance_vert_gs_ctrl1_e&type=2button&s=" +symbolslist[i] +"%2C"
-	htmlfile = urllib.urlopen(url)
-	htmltext = htmlfile.read()
-	regex = '<span id="yfs_l84_' +symbolslist[i] +'">(.+?)</span>'
-	pattern = re.compile(regex)
-	price = re.findall(pattern,htmltext)
-	price1 = ', '.join(price)
-	price2 = float(price1)
-	procent = ((price2 - orgpricelist[i]) / price2)*100
-	print "<tr>"
-	print "<td>",symbolslist[i],"</td>"
-	print "<td>",price2,"</td>"
-	print "<td>",procent,"</td>"
-	print "</tr>"
-	i+=1
-print "test"
-print '</table>'
-print "</body>"
-print "</html>"
-
+    url = "https://www.google.com/finance?q="+symbolslist[i]
+    htmlfile = urllib.urlopen(url)
+    htmltext = htmlfile.read()
+    regex = '<span id="ref_(.+?)_l">(.+?)</span>'
+    pattern = re.compile(regex)
+    #pattern = re.compile(regex)
+    price = re.findall(pattern,htmltext)
+    print price
+    i+=1
